9e4b7cf64eecca125960254b8c25910a92167c3d
shelldonryan <shelldon.boy15@gmail.com>
2026-04-19 17:18:49 -0300

    v2.0.3

--- main.py
+++ main.py
@@ -6,38 +6,61 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from scripts.collector import collect_orderbook
 from scripts.collector_result import collect_recent_results
+from utils.db_client import init_db, init_results_table
 scheduler = BackgroundScheduler()
 
-scheduler.add_job(
-    collect_orderbook,
-    IntervalTrigger(seconds=60),
-    id="collector_job",
-    name="Collector",
-    replace_existing=True
-)
+def initialize_databases():    
+    try:
+        print("  ⏳ Inicializando tabela de market_data...", end=" ", flush=True)
+        init_db()
+        print("✅ OK\n")
+    except Exception as e:
+        print(f"❌ ERRO: {e}\n")
+        return False
+    
+    try:
+        print("  ⏳ Inicializando tabela de resultados...", end=" ", flush=True)
+        init_results_table()
+        print("✅ OK\n")
+    except Exception as e:
+        print(f"❌ ERRO: {e}\n")
+        return False
+    
+    return True
 
-scheduler.add_job(
-    collect_recent_results,
-    IntervalTrigger(seconds=180),
-    id="result_collector_job",
-    name="Result Collector",
-    replace_existing=True
-)
+init = initialize_databases()
 
-scheduler.add_job(
-    lambda: print("[INFO] Validação agendada"),
-    IntervalTrigger(hours=1),
-    id="validation_job",
-    name="Validation",
-    replace_existing=True
-)
+if init:
+    scheduler.add_job(
+        collect_orderbook,
+        IntervalTrigger(seconds=60),
+        id="collector_job",
+        name="Collector",
+        replace_existing=True
+    )
 
-scheduler.start()
+    scheduler.add_job(
+        collect_recent_results,
+        IntervalTrigger(seconds=180),
+        id="result_collector_job",
+        name="Result Collector",
+        replace_existing=True
+    )
 
-print("[INFO] Scheduler iniciado")
+    scheduler.add_job(
+        lambda: print("[INFO] Validação agendada"),
+        IntervalTrigger(hours=1),
+        id="validation_job",
+        name="Validation",
+        replace_existing=True
+    )
 
-try:
-    while True:
-        pass
-except KeyboardInterrupt:
-    scheduler.shutdown()
+    scheduler.start()
+
+    print("[INFO] Scheduler iniciado")
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        scheduler.shutdown()
